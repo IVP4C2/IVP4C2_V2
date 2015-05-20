3ff6d2b9efede668ffdde78b99beb87dc42ad66d
Bram Reinold <bhnreino@avans.nl>
2015-05-20 19:38:41 +0200

    met afsluit handler

--- src/main/java/nl/edu/avans/ivp4c2/main/Main.java
+++ src/main/java/nl/edu/avans/ivp4c2/main/Main.java
@@ -4,9 +4,24 @@ import javax.swing.JFrame;
 
 import nl.edu.avans.ivp4c2.manager.BarManager;
 import nl.edu.avans.ivp4c2.presentation.BarGUIFrame;
+
+import static javax.swing.JOptionPane.*;
+
 public class Main{
 	public static void main(String args[] ) {
 		BarManager barmanager = new BarManager();
-		JFrame frame = new BarGUIFrame(barmanager);
-	}
+		final JFrame frame = new BarGUIFrame(barmanager);
+		frame.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent windowEvent) {
+				if (showConfirmDialog(frame,
+						"Weet u zeker dat u de applicatie wilt sluiten?",
+						"Applicatie afsluiten?",
+						YES_NO_OPTION,
+						QUESTION_MESSAGE) == YES_OPTION) {
+					System.exit(0);
+				}
+			}
+		});
+}
 }
--- src/main/java/nl/edu/avans/ivp4c2/presentation/BarGUIFrame.java
+++ src/main/java/nl/edu/avans/ivp4c2/presentation/BarGUIFrame.java
@@ -1,20 +1,23 @@
 package nl.edu.avans.ivp4c2.presentation;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import nl.edu.avans.ivp4c2.manager.BarManager;
+
+import static javax.swing.JOptionPane.*;
 
 public class BarGUIFrame extends JFrame {
 	BarManager barmanager;
 	
 	public BarGUIFrame(BarManager barmanager) {
 		this.barmanager = barmanager;
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 		this.setTitle("Bedieningsysteem");
 		this.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		JPanel panel = new BarGUI(barmanager);
 		this.setContentPane(panel);
 		this.setVisible(true);
-	}
+
+
+}
 }
